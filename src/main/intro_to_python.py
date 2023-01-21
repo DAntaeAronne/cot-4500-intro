22acdc0b1ecee2cc841eb7e152339a5987cd440e
DAntaeAronne <94494773+DAntaeAronne@users.noreply.github.com>
2023-01-20 23:02:14 -0500

    Update intro_to_python.py

--- src/main/intro_to_python.py
+++ src/main/intro_to_python.py
@@ -1,1 +1,23 @@
+import numpy as np
+import pandas as pd
 
+a = np.zeros((3, 3))
+
+for i in range(3):
+  for j in range(3):
+    if i == j:
+      a[i,j] = 1
+      
+print(f"{a}\n\n")
+
+for i in range(3):
+  for j in range(3):
+    if i != j:
+      a[i,j] += 3
+      
+      
+print(f"{a}\n\n")
+
+a = np.delete(a, 1, 1)
+
+print(f"{a}")
